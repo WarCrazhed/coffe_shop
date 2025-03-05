92564d401e074cd1d967361e8bb6951468d4559f
Mario Zamora <mariozamora5126@gmail.com>
2025-03-05 08:06:47 -0600

    DjAngo Admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Product
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ['name', 'description', 'price']
+    list_filter = ['available']
+    search_fields = ['name', 'description', 'price']
+    
+admin.site.register(Product, ProductAdmin)
