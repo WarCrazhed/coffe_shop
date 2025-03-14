f084d5b9dd814c9bcaaf38ed71569d8bc243d04c
Mario Zamora <mariozamora5126@gmail.com>
2025-03-14 08:55:05 -0600

    Install django-environ

--- coffe_shop/settings.py
+++ coffe_shop/settings.py
@@ -1,4 +1,5 @@
 import os
+import environ
 """
 Django settings for coffe_shop project.
 
@@ -15,8 +16,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
